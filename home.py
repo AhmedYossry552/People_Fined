fadcbbeb2e6926cc6239e82beb127bfd24a0741b
Ahmed Yossry <ahmedyossry552@gmail.com>
2024-06-28 05:05:01 +0300

    as

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,  request, session, g
 from common_imports import *
+
 from database import  get_connection
 home_bp = Blueprint('home', __name__)
 
@@ -12,6 +13,7 @@ def H_lost():
     cur = mysql.cursor()
     cur.execute("SELECT * FROM lost_people ORDER BY id DESC LIMIT 5")
     rows = cur.fetchall()
+   
         #return jsonify({'message': rows})
           
         # Convert rows to a list of dictionaries for JSON response
@@ -37,7 +39,7 @@ def H_lost():
     # Extract the image path from the item
              image_path = item['png_ref']
     # Construct the image URL and add it to the item
-             item['image_url'] = f"http://192.168.1.19:5000/get_image/{image_path}"
+             item['image_url'] =url_for('report.get_photo', filename=image_path)
 
     mysql.commit()
          
@@ -49,7 +51,9 @@ def H_find():
     mysql =g.db
     cur = mysql.cursor()
     cur.execute("SELECT * FROM find_people ORDER BY id DESC LIMIT 5")
+   
     rows = cur.fetchall()
+   
         #return jsonify({'message': rows})
           
         # Convert rows to a list of dictionaries for JSON response
@@ -76,17 +80,17 @@ def H_find():
             image_path = item['png_ref']
 
     # Construct the image URL and add it to the item
-            item['image_url'] = f"http://192.168.1.19:5000/get_image/{image_path}"        
+            item['image_url'] = url_for('report.get_photo', filename=image_path)        
     return jsonify(results)  
 
 
-@home_bp.route('/home_1', methods=['POST'])
+#@home_bp.route('/home_1', methods=['POST'])
 
-def send_push_notification():
-    from pyfcm import FCMNotification
-    push_service = FCMNotification(api_key="AAAAOgpzEww:APA91bH5E6zMNhbVZHHyepP2m0MDzOCdYMV3P1prvL_fimLMujYj8r6AGTuJZX5R0HQeOZiEu-rjKcNsUO_MLmxtX-braubUv2_lL5zxvvdMoFsbM2izJWbYQPzdBsW88qTb6nFQ8Ta-")
-    result = push_service.notify_single_device(registration_id="ARVxEKGb7eejNLCFEtPXijiWHl02z638ySYRM9jUEVT3q21XtKh9rxqeCwX0yXeLXSwnLE", message_title="ahmedyossry", message_body="ahmedyossry")
-    return result
+#def send_push_notification():
+    #from pyfcm import FCMNotification
+    #push_service = FCMNotification(api_key="AAAAOgpzEww:APA91bH5E6zMNhbVZHHyepP2m0MDzOCdYMV3P1prvL_fimLMujYj8r6AGTuJZX5R0HQeOZiEu-rjKcNsUO_MLmxtX-braubUv2_lL5zxvvdMoFsbM2izJWbYQPzdBsW88qTb6nFQ8Ta-")
+   # result = push_service.notify_single_device(registration_id="ARVxEKGb7eejNLCFEtPXijiWHl02z638ySYRM9jUEVT3q21XtKh9rxqeCwX0yXeLXSwnLE", message_title="ahmedyossry", message_body="ahmedyossry")
+   # return result
 
 
 
